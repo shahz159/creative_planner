c9498fbdb43f0eddeb774e562ef488b19c63efbc
v.maharshi <v.maharshi@DESIGNER3>
2023-03-15 12:55:57 +0530

    UI Changes Timeline

--- src/app/_LayoutDashboard/dashboard/dashboard.component.ts
+++ src/app/_LayoutDashboard/dashboard/dashboard.component.ts
@@ -3594,7 +3594,7 @@ export class DashboardComponent implements OnInit {
     document.getElementById("main-foot").classList.toggle("overflow-hidden");
   }
 
-  notinAction() {
-    this.notifyService.showError("Development Under Maintainance", 'Failed');
-  }
+  // notinAction() {
+  //   this.notifyService.showError("Development Under Maintainance", 'Failed');
+  // }
 }
