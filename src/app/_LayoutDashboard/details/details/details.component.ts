48ede6314d827ce27072c6cdb8ed87871a323695
v.maharshi <v.maharshi@DESIGNER3>
2023-10-09 11:28:17 +0530

    pull latest

--- src/app/_LayoutDashboard/details/details/details.component.ts
+++ src/app/_LayoutDashboard/details/details/details.component.ts
@@ -10,6 +10,26 @@ export class DetailsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    var lang = {
+      "javascript": "70%",
+    };
+    
+    var multiply = 4;
+    
+    $.each( lang, function( language, pourcent) {
+    
+      var delay = 700;
+      
+      setTimeout(function() {
+        $('#'+language+'-pourcent').html(pourcent);
+      },delay*multiply);
+      
+      multiply++;
+    
+    });
+    
   }
+  
 
 }
