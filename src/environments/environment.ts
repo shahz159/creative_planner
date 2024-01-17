ddfd1e99ca01b83908e99052ab9bb60e0308e30a
Shiva400149 <92504474+Shiva400149@users.noreply.github.com>
2024-01-17 19:33:37 +0530

    pull latest

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ export const environment = {
   production: false,
   //localServer
   // baseUrl:'http://localhost:1221/api/'
-  baseUrl:'https://cswebapps.com/PortfolioAPI/api/'
+  // baseUrl:'https://cswebapps.com/PortfolioAPI/api/'
+  baseUrl:'https://cswebapps.com/TestPortfolioAPI/api/'
 //DMS Live Server
 //baseUrl:'http://localhost:1221/api/'
   //MyMachine Local IIS Server
