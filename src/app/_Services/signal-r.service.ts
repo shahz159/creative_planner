0ce0f8fcd86e4c96d6f0c5da81c236339220d195
Waseem912 <108920703+Waseem912@users.noreply.github.com>
2024-03-07 18:55:27 +0530

    latets

--- src/app/_Services/signal-r.service.ts
+++ src/app/_Services/signal-r.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import * as signalR from "@microsoft/signalr";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignalRService {
+  private hubConnection: signalR.HubConnection;
 
-  constructor() { }
+  constructor() { 
+    this.hubConnection = new signalR.HubConnectionBuilder()
+      .withUrl("https://your-server-url/myHub")
+      .configureLogging(signalR.LogLevel.Information)
+      .build();
+
+    this.startConnection();
+  }
+
+  private startConnection() {
+    this.hubConnection.start()
+      .then(() => console.log('Connection started'))
+      .catch(err => console.log('Error while starting connection: ' + err));
+  }
+
+  public sendMessage(message: string) {
+    this.hubConnection.invoke('SendMessage', message)
+      .catch(err => console.error(err));
+  }
+
+  public addReceiveMessageListener(callback: (message: string) => void) {
+    this.hubConnection.on('ReceiveMessage', callback);
+  }
 }
