3ae8051baae3a635f484212e9466f3f7177cf405
Waseem912 <108920703+Waseem912@users.noreply.github.com>
2024-03-13 09:36:52 +0530

    event edit

--- src/app/_LayoutDashboard/meeting-report/meeting-report.component.ts
+++ src/app/_LayoutDashboard/meeting-report/meeting-report.component.ts
@@ -1148,6 +1148,7 @@ export class MeetingReportComponent implements OnInit {
   sched_date: any;
   notes_data: any;
   Notes: any = [];
+  
   GetPreviousdate_meetingdata() {
     this.Schedule_ID = this.Scheduleid;
     this._calenderDto.Schedule_ID = this.Schedule_ID;
